21dcca646a84e1e3039c107502b13e43f7283e28
Adrian Ortmann <adrian.ortmann@online.de>
2013-06-16 19:43:34 +0200

    cleaned it

--- MedienBibliothek/Controller/WriteExcelFileHelper.cs
+++ MedienBibliothek/Controller/WriteExcelFileHelper.cs
@@ -9,8 +9,7 @@ namespace MedienBibliothek.Controller
 {
     class WriteExcelFileHelper
     {
-//        public Application ExcelWriter = new Microsoft.Office.Interop.Excel.Application();
-        Microsoft.Office.Interop.Excel.Application _excelWriterApp = new Microsoft.Office.Interop.Excel.Application();
+        readonly Microsoft.Office.Interop.Excel.Application _excelWriterApp = new Microsoft.Office.Interop.Excel.Application();
         Microsoft.Office.Interop.Excel.Worksheet _wsheet;
         Microsoft.Office.Interop.Excel.Workbook _wbook;
         public void WriteVideoListToExcelFile(IEnumerable<Video> videoList)
@@ -23,14 +22,8 @@ namespace MedienBibliothek.Controller
             }
             _excelWriterApp.Visible = false;
             _excelWriterApp.DisplayAlerts = false;
-//            var workbook = ExcelWriter.Workbooks.Add(1);
-//            var worksheet = (Worksheet) workbook.Sheets[1];
-//            ExcelWriter.Worksheets.Add(1);
-//            workbook.Open(Properties.Settings.Default.excelPath);
             _wbook = _excelWriterApp.Workbooks.Add(true);
             _wsheet = (Worksheet)_wbook.ActiveSheet;
-//            _wbook.Open(Properties.Settings.Default.excelPath);
-
             WriteVideoList(videoList);
         }
 
@@ -44,22 +37,8 @@ namespace MedienBibliothek.Controller
                 WriteValueInCell(3, counter, video.Path);
                 counter++;
             }
-//            _excelWriterApp.Save(Properties.Settings.Default.excelPath);
             _wbook.SaveAs(Properties.Settings.Default.excelPath);
-//            _wbook.Save();
             _wbook.Close();
-//            _excelWriterApp.Workbooks.Close();
-            
-//            ExcelWriter.Workbooks.Close();
-//            ExcelWriter.Save("VideoList.xls");
-            
-//            ExcelWriter.Workbooks.Close();
-//            ExcelWriter.SaveAs("VideoList.xls");
-//            ExcelWriter.
-//            ExcelWriter.Save(Properties.Settings.Default.excelPath);
-//            ExcelWriter.Sa
-//            ExcelWriter.Workbooks.Close();
-//            ExcelWriter.Quit();
         }
 
         private void WriteValueInCell(int column, int row, string value)
