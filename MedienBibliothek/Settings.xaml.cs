5c93f1add2555609edb8d43dd9eb4600d02bbc3f
Adrian Ortmann <adrian.ortmann@online.de>
2013-07-03 20:06:07 +0200

    changed settings page for excel file and jd download path

--- MedienBibliothek/Settings.xaml.cs
+++ MedienBibliothek/Settings.xaml.cs
@@ -20,8 +20,12 @@ namespace MedienBibliothek
     /// </summary>
     public partial class Settings : Window
     {
-        private readonly FolderBrowserDialog browseVideoPath = new FolderBrowserDialog();
-        private OpenFileDialog browseVlcExecPath = new OpenFileDialog();
+        private readonly FolderBrowserDialog _browseVideoPath = new FolderBrowserDialog();
+        private readonly OpenFileDialog _browseVlcExecPath = new OpenFileDialog();
+
+        private readonly FolderBrowserDialog _browseJdownloaderDownloadPath = new FolderBrowserDialog();
+        private readonly OpenFileDialog _browseExcelFilePath = new OpenFileDialog();
+
         public Settings()
         {
 
@@ -29,6 +33,8 @@ namespace MedienBibliothek
             InitializeComponent();
             VideoFolderPathBox.Text = Properties.Settings.Default.videoPath;
             VlcExecPathBox.Text = Properties.Settings.Default.vlcFilePath;
+            JdownloaderDownloadPathBox.Text = Properties.Settings.Default.jdownloaderVideoPath;
+            ExcelFilePathBox.Text = Properties.Settings.Default.excelFile;
         }
 
         
@@ -54,6 +60,20 @@ namespace MedienBibliothek
             {
                 Properties.Settings.Default.vlcFilePath = VlcExecPathBox.Text;
             }
+            if (String.IsNullOrEmpty(JdownloaderDownloadPathBox.Text))
+            {
+            }
+            else
+            {
+                Properties.Settings.Default.jdownloaderVideoPath = JdownloaderDownloadPathBox.Text;
+            }
+            if (String.IsNullOrEmpty(ExcelFilePathBox.Text))
+            {
+            }
+            else
+            {
+                Properties.Settings.Default.excelFile = ExcelFilePathBox.Text;
+            }
             Properties.Settings.Default.Save();
             Close();
 
@@ -61,14 +81,26 @@ namespace MedienBibliothek
 
         private void BrowseVideoPathClick(object sender, RoutedEventArgs e)
         {
-            browseVideoPath.ShowDialog();
-            VideoFolderPathBox.Text = browseVideoPath.SelectedPath;
+            _browseVideoPath.ShowDialog();
+            VideoFolderPathBox.Text = _browseVideoPath.SelectedPath;
         }
 
         private void BrowseVlcExecPathClick(object sender, RoutedEventArgs e)
         {
-            browseVlcExecPath.ShowDialog();
-            VlcExecPathBox.Text = browseVlcExecPath.InitialDirectory + browseVlcExecPath.FileName;
+            _browseVlcExecPath.ShowDialog();
+            VlcExecPathBox.Text = _browseVlcExecPath.InitialDirectory + _browseVlcExecPath.FileName;
+        }
+
+        private void BrowseExcelFilePathClick(object sender, RoutedEventArgs e)
+        {
+            _browseExcelFilePath.ShowDialog();
+            ExcelFilePathBox.Text = _browseExcelFilePath.InitialDirectory + _browseExcelFilePath.FileName;
+        }
+
+        private void BrowseJdownloaderDownloadPathClick(object sender, RoutedEventArgs e)
+        {
+            _browseJdownloaderDownloadPath.ShowDialog();
+            JdownloaderDownloadPathBox.Text = _browseJdownloaderDownloadPath.SelectedPath;
         }
     }
 }
